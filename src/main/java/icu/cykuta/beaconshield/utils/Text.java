5ebc9cde35f83711094141004e5dcf3cc5a8f489
Mau <mautdhd@gmail.com>
2025-03-13 16:47:18 -0300

    feat: Hex color support

--- src/main/java/icu/cykuta/beaconshield/utils/Text.java
+++ src/main/java/icu/cykuta/beaconshield/utils/Text.java
@@ -1,9 +1,29 @@
 package icu.cykuta.beaconshield.utils;
 
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Text {
 
-    public static String color(String text) {
-        return text.replace("&", "§");
+    public static String color(String message) {
+        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
+
+            char[] ch = replaceSharp.toCharArray();
+            StringBuilder builder = new StringBuilder("");
+            for (char c : ch) {
+                builder.append("&" + c);
+            }
+
+            message = message.replace(hexCode, builder.toString());
+            matcher = pattern.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 
     public static String stripColor(String message) {
